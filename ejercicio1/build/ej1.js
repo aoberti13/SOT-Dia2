65d1475d909219137d97600770ab0afb6b103ccb
aoberti13 <92743567+aoberti13@users.noreply.github.com>
2021-10-18 21:22:23 -0300

    Add files via upload

---
+++ ejercicio1/build/ej1.js
@@ -0,0 +1,19 @@
+"use strict";
+
+/*
+Realizar un proyecto usando Babel que haga lo siguiente
+
+
+Tenga una función que genere números aleatorios entre 2 rangos pasados por parámetros
+
+
+Tenga una función que genere el un objeto con 3 keys (rojo, verde y azul). El valor de cada key sera un numero aleatorio entre 0 y 255;
+
+
+Ejecutar el programa con nodemon y comprobar que la salida sea la esperada.
+Ejecutar el buildeo de la aplicacion. Ejecutar el codigo transpilado y comprobar que la salida sea la esperada
+*/
+const functions = require('./functions');
+
+console.log(functions.randomNum(1, 20, 50, 100));
+console.log(functions.randomColour());
---
+++ ejercicio1/src/functions.js
@@ -0,0 +1,28 @@
+const randomNum = (min, max, min1, max1) => {
+    let rango1 = Math.floor((Math.random() * (max - min)) + min);
+    let rango2 = Math.floor((Math.random() * (max1 - min1)) + min1);
+    let bool = Math.floor((Math.random() * (2 - 0)) + 0);
+    if (bool === 1) {
+        return rango1;
+    }
+    return rango2;
+};
+
+const randomColour = () => {
+    let colour = {};
+    let numbers = [];
+    for (let i = 0; i < 3; i++) {
+        const random = Math.floor(Math.random() * (255));
+        numbers.push(random);
+    };
+    colour.red = numbers[0];
+    colour.green = numbers[1];
+    colour.blue = numbers[2];
+    return colour;
+};
+
+
+module.exports = {
+    randomNum,
+    randomColour
+};
